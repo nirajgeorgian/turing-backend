13e0478bde8d78b0039f3fde28dd3760d012fb21
nirajgeorgian <nirajgeorgian@gmail.com>
2019-02-23 13:32:55 +0530

    added docker-compose file

--- scripts/mysql/init_db.sql
+++ scripts/mysql/init_db.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE IF NOT EXISTS ecomm;
-CREATE USER 'ecomm'@'%' IDENTIFIED BY 'dodo';
-GRANT ALL PRIVILEGES ON ecomm.* TO 'ecomm'@'%';
+CREATE USER 'ecomm'@'localhost' IDENTIFIED BY 'dodo';
+GRANT ALL PRIVILEGES ON ecomm.* TO 'ecomm'@'localhost';
