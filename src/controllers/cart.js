efcdb09889495ba0305b9346cf8b2b4cb21c878f
nirajgeorgian <nirajgeorgian@gmail.com>
2019-02-24 04:46:28 +0530

    added cart delete option

--- src/controllers/cart.js
+++ src/controllers/cart.js
@@ -1,4 +1,4 @@
-import { shopping_cart, sequelize } from '../db/models'
+import { shopping_cart, sequelize, product } from '../db/models'
 import { errorMessage, successMessage } from '../utils/response'
 
 export const getCart = async (req, res) => {
@@ -20,20 +20,95 @@ export const addToCart = async (req, res) => {
 	const added_on = sequelize.literal('CURRENT_TIMESTAMP')
 	const attribute = req.body.attribute ? req.body.attribute.trim() : null
 	const quantity = req.body.quantity ? req.body.quantity : null
+	const single_product = await product.findByPk(product_id)
+	if (single_product) {
+		const previous_cart = await shopping_cart.findOne({
+			where: { product_id, customer_id },
+		})
+		if (previous_cart) {
+			return res
+				.status(400)
+				.send(errorMessage('item is already added to the cart'))
+		} else {
+			try {
+				await shopping_cart.create({
+					customer_id,
+					product_id,
+					buy_now,
+					added_on,
+					attribute,
+					quantity,
+				})
+				const carts = await shopping_cart.findAll({
+					where: { customer_id },
+				})
+				return res.send(successMessage('cart', carts))
+			} catch (err) {
+				return res.status(400).send(errorMessage(err.message))
+			}
+		}
+	} else {
+		return res.status(400).send(errorMessage('sorry product is not available'))
+	}
+}
 
+export const updateCart = async (req, res) => {
+	const customer_id = req.user.id
+	const cart_id = req.body.cart_id ? Number(req.body.cart_id) : null
+	const product_id = req.params.product_id
+	if (!cart_id) {
+		return res.status(400).send(errorMessage('please pass cart_id'))
+	}
 	try {
-		await shopping_cart.create({
-			customer_id,
-			product_id,
-			buy_now,
-			added_on,
-			attribute,
-			quantity,
+		const previous_cart = await shopping_cart.findOne({
+			where: { product_id, customer_id },
 		})
-		const carts = await shopping_cart.findAll({
-			where: { customer_id },
+		if (previous_cart) {
+			const updated_cart = await shopping_cart.update(req.body, {
+				where: { cart_id, product_id, customer_id },
+			})
+			if (updated_cart) {
+				const carts = await shopping_cart.findAll({
+					where: { customer_id },
+				})
+				return res.send(successMessage('cart', carts))
+			} else {
+				return res.status(400).send(errorMessage('error on cart update data'))
+			}
+		} else {
+			return res.status(400).send(errorMessage('please pass proper product id'))
+		}
+	} catch (err) {
+		return res.status(400).send(errorMessage(err.message))
+	}
+}
+
+export const removeCart = async (req, res) => {
+	const customer_id = req.user.id
+	const cart_id = req.body.cart_id ? Number(req.body.cart_id) : null
+	const product_id = req.params.product_id
+	if (!cart_id) {
+		return res.status(400).send(errorMessage('please pass cart_id'))
+	}
+	try {
+		const updated_cart = await shopping_cart.findOne({
+			where: {
+				cart_id,
+				product_id,
+				customer_id,
+			},
 		})
-		return res.send(successMessage('cart', carts))
+		if (updated_cart) {
+			await shopping_cart.destroy({
+				where: { cart_id, customer_id, product_id },
+			})
+			const carts = await shopping_cart.findAll({
+				where: { customer_id },
+			})
+			return res.send(successMessage('cart', carts))
+		} else {
+			return res.status(400).send(errorMessage('please pass proper product id'))
+		}
 	} catch (err) {
 		return res.status(400).send(errorMessage(err.message))
 	}
--- src/routes/cart.js
+++ src/routes/cart.js
@@ -1,9 +1,13 @@
 import express from 'express'
-import { getCart, addToCart } from '../controllers/cart'
+import { getCart, addToCart, updateCart, removeCart } from '../controllers/cart'
 import { authenticated } from '../utils/auth/auth.util'
 
 const router = express.Router()
 router.route('/cart').get(authenticated, getCart)
-router.route('/cart/:product_id').post(authenticated, addToCart)
+router
+	.route('/cart/:product_id')
+	.post(authenticated, addToCart)
+	.put(authenticated, updateCart)
+	.delete(authenticated, removeCart)
 
 export default router
